7b492c90b103aa8fd55c214726fb80326702fd62
Jan Ufniarz <jan.ufniarz@gmail.com>
2023-04-05 14:40:16 +0200

    map issue solved

--- windows/runner/flutter_window.cpp
+++ windows/runner/flutter_window.cpp
@@ -4,10 +4,87 @@
 
 #include "flutter/generated_plugin_registrant.h"
 
+#include <flutter/binary_messenger.h>
+#include <flutter/standard_method_codec.h>
+#include <flutter/method_channel.h>
+#include <flutter/method_result_functions.h>
+
+#include <any>
+#include <map>
+#include <string>
+#include <flutter/flutter_view.h>
+
 FlutterWindow::FlutterWindow(const flutter::DartProject& project)
     : project_(project) {}
 
 FlutterWindow::~FlutterWindow() {}
+
+
+// ===========================================
+// call.arguments -> map conversion
+std::map<std::string, std::any> methodCallToMap(const flutter::MethodCall<flutter::EncodableValue>& call) {
+    std::map<std::string, std::any> result;
+    auto arguments = call.arguments();
+    if (std::holds_alternative<flutter::EncodableMap>(*arguments)) {
+        flutter::EncodableMap map = std::get<flutter::EncodableMap>(*arguments);
+        // Iterate over the key-value pairs of the map
+        for (const auto& pair : map) {
+            const std::string& key = std::get<std::string>(pair.first);
+            const flutter::EncodableValue& value = pair.second;
+            // Convert the value to an any object and insert it into the result map
+            if (std::holds_alternative<std::string>(value)) {
+                result[key] = std::get<std::string>(value);
+            } else if (std::holds_alternative<int>(value)) {
+                result[key] = std::get<int>(value);
+            } else if (std::holds_alternative<double>(value)) {
+                result[key] = std::get<double>(value);
+            } else if (std::holds_alternative<bool>(value)) {
+                result[key] = std::get<bool>(value);
+            } else {
+                // Unsupported argument type
+                result[key] = std::any();
+            }
+        }
+    }
+    return result;
+}
+
+
+
+// ===========================================
+
+// ===========================================
+// MethodCannel
+void initMethodChannel(flutter::FlutterEngine* flutter_instance) {
+    // name your channel
+    const static std::string channel_name("com.flutter.main/Channel");
+
+    auto channel =
+        std::make_unique<flutter::MethodChannel<>>(
+            flutter_instance->messenger(), channel_name,
+            &flutter::StandardMethodCodec::GetInstance());
+
+    channel->SetMethodCallHandler(
+        [](const flutter::MethodCall<>& call, 
+          std::unique_ptr<flutter::MethodResult<>> result) {
+
+            std::map<std::string, std::any> arguments = methodCallToMap(call);
+
+            // cheack method name called from dart
+            if (call.method_name().compare("respond") == 0) {
+            // do whate ever you want
+
+            std::string res = std::any_cast<std::string>(arguments.at("prompt"));
+
+            result->Success(res);
+            }
+            else {
+                result->NotImplemented();
+            }
+          }
+  );
+}
+// ===========================================
 
 bool FlutterWindow::OnCreate() {
   if (!Win32Window::OnCreate()) {
@@ -25,6 +102,12 @@ bool FlutterWindow::OnCreate() {
     return false;
   }
   RegisterPlugins(flutter_controller_->engine());
+  // ===========================================
+  // initialize method channel here 
+    initMethodChannel(flutter_controller_->engine());
+
+    //?run_loop_->RegisterFlutterInstance(flutter_controller_->engine());
+  // ===========================================
   SetChildContent(flutter_controller_->view()->GetNativeWindow());
 
   flutter_controller_->engine()->SetNextFrameCallback([&]() {
