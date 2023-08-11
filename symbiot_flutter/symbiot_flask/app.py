6de4afb1b86443dcebde1e3b4e61565ae83e8b18
Mikolaj Kowal <Mikolaj.kowal5780@gmail.com>
2023-08-11 21:04:39 +0200

    app.py3

--- symbiot_flutter/symbiot_flask/app.py
+++ symbiot_flutter/symbiot_flask/app.py
@@ -1,15 +1,24 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+class AsciiView:
+    def __init__(self, method):
+        self.method = method
 
-@app.route("/")
-def get():
-    response = {
-        'message': 'hello'
-    }
-    return jsonify(response)
+    @staticmethod
+    def get_output(inputchr):
+        return {'output': str(ord(inputchr))}
 
+    def process_request(self):
+        inputchr = str(request.args.get('query', ''))
+        return self.get_output(inputchr)
+
+@app.route('/api', methods=['GET', 'PUT', 'DELETE', 'POST'])
+def handle_ascii():
+    method = request.method
+    view = AsciiView(method)
+    return jsonify(view.process_request())
 
 if __name__ == '__main__':
     app.run()
