095e30800e4cb260de1037b5333f1f0d73a330e7
Jan Ufniarz <jan.ufniarz@gmail.com>
2023-10-01 15:00:27 +0200

    operation view cd

--- symbiot_flutter/lib/views/operation_view.dart
+++ symbiot_flutter/lib/views/operation_view.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:symbiot_flutter/controllers/operation_controller.dart';
 
+import '../palette.dart';
+import '../widgets/input_bar.dart';
 import '../widgets/symbiot_scaffold.dart';
 
 class OperationView extends StatelessWidget {
@@ -11,12 +13,32 @@ class OperationView extends StatelessWidget {
   Widget build(BuildContext context) => Consumer<OperationController>(
     builder: (context, controller, child) => SymbiotScaffold(
       tittle: controller.model!.name,
-      body: SingleChildScrollView(
-        child: Row(
-          children: const <Widget>[
-
-          ],
-        ),
+      body: Column(
+        children: [
+          InputBar(
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: Text(
+                        "The main purpose of this operation is:\n"
+                        "${controller.model!.nordStar}",
+                        style: const TextStyle(
+                          color: Palette.accent,
+                          fontSize: 25,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     ),
   );
--- symbiot_flutter/lib/widgets/input_bar.dart
+++ symbiot_flutter/lib/widgets/input_bar.dart
@@ -3,39 +3,50 @@ import 'package:flutter/material.dart';
 import '../palette.dart';
 
 class InputBar extends StatelessWidget {
-  const InputBar({super.key});
+  final Widget child;
+
+  const InputBar({super.key, required this.child});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.bottomCenter,
-      padding: const EdgeInsets.symmetric(
-          horizontal: 70,
-          vertical: 20
-      ),
-      child: Row(
+    return Expanded(
+      child: Stack(
         children: <Widget>[
 
-          Flexible(
-            child: TextField(
-              decoration: const InputDecoration(
-                filled: true,
-                fillColor: Palette.accent,
-                hintText: 'Wpisz wiadomość...',
-              ),
-              onChanged: (text) {},
-              onSubmitted: (text) {},
-              maxLines: null,
+          child,
+
+          Container(
+            alignment: Alignment.bottomCenter,
+            padding: const EdgeInsets.symmetric(
+                horizontal: 70,
+                vertical: 20
+            ),
+            child: Row(
+              children: <Widget>[
+
+                Flexible(
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      filled: true,
+                      fillColor: Palette.accent,
+                      hintText: 'Wpisz wiadomość...',
+                    ),
+                    onChanged: (text) {},
+                    onSubmitted: (text) {},
+                    maxLines: null,
+                  ),
+                ),
+
+                IconButton(
+                  iconSize: 30,
+                  color: Palette.primary,
+                  icon: const Icon(Icons.send),
+                  onPressed: () {},
+                ),
+              ],
             ),
           ),
-
-          IconButton(
-            iconSize: 30,
-            color: Palette.primary,
-            icon: const Icon(Icons.send),
-            onPressed: () {},
-          ),
-        ],
+        ]
       ),
     );
   }
