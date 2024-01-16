ce4f797f626bc79405fec916ba1940a8d8c2ef5b
Jan Ufniarz <jan.ufniarz@email.com>
2024-01-16 13:05:34 +0100

    database container

--- symbiot_server/run_server.py
+++ symbiot_server/run_server.py
@@ -1,3 +1,5 @@
+import sys
+
 from flask import Flask
 
 from symbiot_server.config.divisions.client_division import ClientDivision
@@ -8,12 +10,14 @@ from symbiot_server.config.server_starter import ServerStarter
 app: Flask = Flask(__name__)
 
 if __name__ == '__main__':
+    db_host: str = 'symbiot-database-1' if '--docker' in sys.argv else 'localhost'
+
     symbiot_server: ServerStarter = ServerStarter().flask(
         app
     ).sql_alchemy(
         db,
-        'postgresql://postgres:postgres@localhost:5432/symbiot'
+        f'postgresql://postgres:postgres@{db_host}:5432/symbiot'
     ).divisions([
             OperationDivision(),
             ClientDivision(),
-    ]).mediator().listen_all().run(debug=True, port=5000)
+    ]).mediator().listen_all().run(debug=True, host='0.0.0.0', port=5000)
