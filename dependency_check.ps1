aa6f8969e628dcca74aeb0013a45080902220b01
Jan Ufniarz <jan.ufniarz@email.com>
2024-01-20 11:45:29 +0100

    better colors and more info in dependency_check.ps1

--- dependency_check.ps1
+++ dependency_check.ps1
@@ -1,31 +1,37 @@
 $ErrorActionPreference = "SilentlyContinue"
 
-$good = "`e[34m[✓]`e[0m"
-$bad = "`e[34m[✗]`e[0m"
+$good = "`e[32m[✓]`e[0m"
+$bad = "`e[31m[✗]`e[0m"
 
 if (Test-Path -Path (Get-Command flutter -ErrorAction SilentlyContinue).Source) {
     Write-Host "$good Flutter installed"
     flutter doctor | Select-String -SimpleMatch "Windows" | ForEach-Object {
-        Write-Host "`e[34m$($_.Line.Substring(0, [Math]::Min(3, $_.Line.Length)))`e[0m$($_.Line.Substring(3))"
+        $sign = $_.Line.Substring(0, [Math]::Min(3, $_.Line.Length))
+        $sign = if ($sign -like "*✓*") {
+            $good
+        } elseif ($sign -like "*✗*") {
+            $bad
+        } else {
+            "`e[33m$sign`e[0m"
+        }
+        Write-Host "$sign$($_.Line.Substring(3))"
     }
 } else { Write-Host "$bad Flutter not installed or not added to PATH" }
 
 if (Test-Path -Path (Get-Command docker -ErrorAction SilentlyContinue).Source) {
     Write-Host "$good Docker installed"
 
-    $info = @{
-        "Client" = $null
-        "Server" = $null
+    $client = try { docker version --format '{{.Client.Version}}' } catch { $null }
+    $server = try { docker version --format '{{.Server.Version}}' } catch { $null }
+
+    if ($client) {
+        Write-Host "$good Docker Client working. version: $client"
+    } else {
+        Write-Host "$bad Docker Client not working`n`t probably some instalation error"
     }
-
-    try { $info["Client"] = docker version --format '{{.Client.Version}}' } catch { }
-    try { $info["Server"] = docker version --format '{{.Server.Version}}' } catch { }
-
-    foreach ($key in $info.Keys) {
-        if ($info[$key]) {
-            Write-Host "$good Docker $key working. version: $($info[$key])"
-        } else {
-            Write-Host "$bad Docker $key not working"
-        }
+    if ($server) {
+        Write-Host "$good Docker Server working. version: $server"
+    } else {
+        Write-Host "$bad Docker Server not working`n`t try to run docker desktop app"
     }
 } else { Write-Host "$bad Docker not installed or not added to PATH" }
