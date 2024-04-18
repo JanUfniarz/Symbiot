3247e5d556d39d5bc55c1d26098b22e787bd783e
Jan Ufniarz <jan.ufniarz@email.com>
2024-04-18 16:20:08 +0200

    some dead code removed

--- symbiot_server/control/ps_command_generator.py
+++
@@ -1,9 +0,0 @@
-class PSCommandGenerator:
-    @staticmethod
-    def save_to_file(code):
-        path = "python/test.py"
-
-        code = code.replace('"', '\'')
-
-        return f'\"{code}\" | ' \
-            f'Set-Content -Path \"{path}\"'
